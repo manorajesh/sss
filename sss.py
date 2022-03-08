4aef955b02f946ece370db6371b66d4fe1b16ec8
me <mano@raje.sh>
2022-03-08 09:38:50 -0800

    Implemented Reconstruction of secret

--- sss.py
+++ sss.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 from numpy.polynomial import Polynomial
-from scipy.interpolate import lagrange
 import random
 
 ## Prepreparation ##
@@ -20,13 +19,29 @@ def populate_random_list(min_val):
     return random_list
 
 poly = np.polynomial.Polynomial(populate_random_list(min_val))
+print(poly)
 
 for i in range(num_shares):
-    shares_list.append((i, poly(i+1)))
+    shares_list.append((i+1, poly(i+1))) #skip 0 because it's the secret
 print(shares_list)
 
 ## Reconstruction ##
 
-x = np.array([1,2,3])
-y = 1234 + 166*x + 94*x**3
-print(lagrange(x, y))
+given_shares = [(1, 2563.0), (2, 6136.0), (3, 11953.0)]
+
+x = np.zeros(len(given_shares))
+for i in range(len(given_shares)):
+    x[i] = given_shares[i][0]
+print(x)
+
+y = np.zeros(len(given_shares))
+for i in range(len(given_shares)):
+    y[i] = given_shares[i][1]
+print(y)
+
+xp = 0
+yp = 0
+for xi, yi in zip(x, y):
+    yp += yi * np.prod((xp - x[x != xi]) / (xi - x[x != xi]))
+
+print(yp)
